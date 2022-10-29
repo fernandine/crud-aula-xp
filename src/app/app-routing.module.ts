c86157d8d17887af6ee5c306856f8e59b349e9bc
Jean Fernandine <jfernandine@gmail.com>
2022-10-29 14:12:06 -0300

    crud Xp-educacao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { ContactUpdateComponent } from './contact-update/contact-update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'contacts', component: ContactListComponent },
+  {path: 'contacts/create', component: ContactCreateComponent },
+  {path: 'contacts/:id', component: ContactUpdateComponent },
+  {path: '', redirectTo: 'contacts', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
